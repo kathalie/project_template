c7207cca64687b149edac94090248669ab0ad726
Kathryn Verkhohliad <verkhogliadkate@gmail.com>
2024-03-18 21:06:31 +0100

    Added returning bool from write functions

--- app/io/output.py
+++ app/io/output.py
@@ -20,25 +20,33 @@ def write_to_file(contents, filepath):
     Args:
         contents (str): Text to be writen to a file.
         filepath (str): A path to a file
+
+    Returns: Bool. True, if content are successfully written.
     """
     if not isinstance(contents, str):
         print(f'Failed to write {contents}, because it is not of type str!')
-        return
+        return False
 
     with open(filepath, 'w') as file:
         file.write(contents)
+        return True
 
 
-def write_dataframe_to_file(df, filepath):
+def write_dataframe_to_file(df, filepath, index=False):
     """
     Writes pandas.DataFrame to a .csv file by its path.
 
     Args:
         df (DataFrame): DataFrame to be writen to a file.
-        filepath (str): A path to a .csv file
+        filepath (str): A path to a .csv file.
+        index (bool, optional): Write row names (index). Is passed to a to_csv() function
+                                from a library pandas.
+
+    Returns: Bool. True, if DataFrame is successfully written.
     """
     if not isinstance(df, pd.DataFrame):
         print(f'Failed to write, because value of df argument is not pandas.DataFrame!')
-        return
+        return False
 
-    df.to_csv(filepath)
+    df.to_csv(filepath, index=index)
+    return True
