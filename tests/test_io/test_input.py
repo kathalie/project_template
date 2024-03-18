94968526ee216916a1ad09a81b9e7bf7a74a418b
Kathryn Verkhohliad <verkhogliadkate@gmail.com>
2024-03-18 20:43:32 +0100

    Task 5. Covered read_dataframe_from_file with tests.

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -1,4 +1,5 @@
 import app.io.input as i
+import pandas as pd
 
 
 class TestReadFromFile:
@@ -21,4 +22,30 @@ class TestReadFromFile:
         read_content2 = i.read_from_file(filepath)
 
         assert read_content1 == read_content2
+
+
+class TestReadFromFileUsingPandas:
+    def test_valid_data(self):
+        df = pd.DataFrame({'name': ['Raphael', 'Donatello'],
+                           'mask': ['red', 'purple'],
+                           'weapon': ['sai', 'bo staff']})
+        filepath = 'tests/test_data/test.csv'
+        read_df = i.read_dataframe_from_file(filepath)
+
+        print(read_df)
+
+        assert read_df.equals(df)
+
+    def test_invalid_filepath(self):
+        invalid_filepath = './nonexistent_file.csv'
+        read_df = i.read_dataframe_from_file(invalid_filepath)
+
+        assert read_df is None
+
+    def test_content_unchanged(self):
+        filepath = 'tests/test_data/test.csv'
+        read_df1 = i.read_dataframe_from_file(filepath)
+        read_df2 = i.read_dataframe_from_file(filepath)
+
+        assert read_df1.equals(read_df2)
 
