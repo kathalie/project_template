5186488d965d037d3bb2828ced9074511e7b30f9
Kathryn Verkhohliad <verkhogliadkate@gmail.com>
2024-03-18 18:13:51 +0100

    Task 4. Declared functions.

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def read_from_console():
+    pass
+
+
+def read_from_file():
+    pass
+
+
+def read_from_file_using_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,10 @@
+def write_to_console():
+    pass
+
+
+def write_to_file():
+    pass
+
+
+def write_to_file_using_pandas():
+    pass
